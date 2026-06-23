cade90bb0ead8a2221a763014167540f18508814
Arjun Vijay Prakash <arjunv.prakash12345@gmail.com>
2026-06-23 20:51:04 +0530

    redirect to /dashboard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from "next/navigation"
+
 export default function Page() {
-  return (
-    <div className="flex min-h-screen flex-col items-center gap-4 text-gray-400 sm:items-start sm:gap-10">
-      Home
-    </div>
-  )
+  redirect("/dashboard")
 }
